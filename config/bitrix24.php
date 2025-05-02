f5e1d836120bad06b8b8b262128bf2c569ca2a10
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2025-05-02 15:24:08 +0300

    FirstCommit

--- config/bitrix24.php
+++ config/bitrix24.php
@@ -3,8 +3,8 @@
 return [
     'auth' =>[
         'clinet_id' => env('BITRIX24_PHP_SDK_APPLICATION_CLIENT_ID'),
-        'clinet_secret' => env('BITRIX24_PHP_SDK_APPLICATION_CLIENT_SECRET'),
-        'scope' => env('BITRIX24_PHP_SDK_APPLICATION_CLIENT_SECRET'),
+        'clinet_secret' => env('BITRIX24_PHP_SDK_APPLICATION_CLIENT_SECRET' ),
+        'scope' => env('BITRIX24_PHP_SDK_APPLICATION_SCOPE'),
         'portal_domain' => env('BITRIX24_PHP_SDK_PORTAL_DOMAIN'),
         'redirect_oauth_url' => env('BITRIX24_PHP_SDK_REDIRECT_OAUTH_URL'),
     ]
